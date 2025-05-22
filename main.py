7e2d1be9a17cf9d67621d89615378bd55bc827f6
Oscar Gomez <oscargomezsedas@gmail.com>
2025-05-22 19:49:34 +0200

    implement functions to read book text and count words

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(path: str) -> str:
+    with open (path) as f:
+        return f.read()
+
+def count_words(content: str):
+    words = content.split()
+    num_words = len(words)
+    print(num_words, "words found in the document")
+
+def main():
+    count_words(get_book_text("books/frankenstein.txt"))
+    
+main()
